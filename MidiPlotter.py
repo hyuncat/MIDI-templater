e8fd52feda357683a7dd6c004eb3fb8bd4d0cd67
sarah <ssh2198@columbia.edu>
2024-06-19 01:11:04 -0400

    saving progress, not great

--- MidiPlotter.py
+++ MidiPlotter.py
@@ -1,39 +1,47 @@
 import sys
 import pretty_midi
 import pandas as pd
-from PyQt5.QtWidgets import QMainWindow, QApplication, QSlider, QVBoxLayout, QWidget, QPushButton
-from PyQt5.QtCore import Qt, QTimer
+import os
+
+from PyQt6.QtWidgets import QMainWindow, QApplication, QSlider, QVBoxLayout, QWidget, QPushButton
+from PyQt6.QtCore import Qt, QTimer
 import pyqtgraph as pg
 import pygame
 
+from midi2audio import FluidSynth
+from pydub import AudioSegment
+from pydub.playback import play
+import simpleaudio as sa
+
 class MidiPlotter(QMainWindow):
     def __init__(self, midi_file):
-        super().__init__() # Call the parent class (QMainWindow) constructor
-        pygame.mixer.init() # Initialize the pygame mixer
+        super().__init__()  # Call the parent class (QMainWindow) constructor
+        pygame.mixer.init()  # Initialize the pygame mixer
 
-        # Load in a specific MIDI file (later implement file IO)
         self.midi_file = midi_file
+        self.soundfont = 'MuseScore_General.sf3'
+        self.FS = FluidSynth(self.soundfont)
+        
         self.instrument_dfs = self.load_midi_data(midi_file)
-
-        # Initialize the GUI
         self.initUI()
 
-        # Timer variables to update internal state/playback with slider
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_plot_during_playback)
-        self.current_time = 0
+        self.timer.timeout.connect(self.update_slider_with_timer)
+        self.current_time = 0 # in sec
+        self.current_slider = 0 # in ms
         self.is_playing = False
 
+        # self.play_obj = None # simpleaudio play object
 
     def load_midi_data(self, midi_file):
-        """
-        Load in a MIDI file and convert the violin instrument into a dataframe of 
-        pitches, durations, start, and end times for all violin notes.
-        """
-        self.midi_data = pretty_midi.PrettyMIDI(midi_file)
-        pygame.mixer.music.load(midi_file)
+        """Load the MIDI data and convert to a pd.DataFrame for each instrument"""
+        wav_filename = midi_file.replace('.mid', '.wav')
+        if not os.path.exists(wav_filename):
+            self.FS.midi_to_audio(midi_file, wav_filename)
 
-        # Get instrument_dfs of all instruments in MIDI
+        self.midi_data = pretty_midi.PrettyMIDI(midi_file)
+        pygame.mixer.music.load(wav_filename)
+
         instrument_dfs = {}
         for instrument in self.midi_data.instruments:
             rows = []
@@ -42,116 +50,105 @@ class MidiPlotter(QMainWindow):
                 rows.append(row)
             df = pd.DataFrame(rows, columns=['pitch', 'duration', 'start', 'end'])
             instrument_dfs[instrument.program] = df
-
         return instrument_dfs
 
-
     def initUI(self):
-        """
-        Initialize the GUI - setup main window, buttons, plot
-        """
-        # Set up the main window
         self.main_widget = QWidget(self)
         self.setCentralWidget(self.main_widget)
-        layout = QVBoxLayout(self.main_widget)
+        self.layout = QVBoxLayout(self.main_widget)
 
-        # Play button
         self.playButton = QPushButton('Play', self)
         self.playButton.clicked.connect(self.toggle_play)
-        layout.addWidget(self.playButton)
+        self.layout.addWidget(self.playButton)
 
-        # Slider to control playback position
-        self.slider = QSlider(Qt.Horizontal)
+        self.initSlider()
 
-        # Set the slider range to the length of the MIDI file
-        midi_length_seconds = int(self.midi_data.get_end_time())
-        self.slider.setMaximum(midi_length_seconds)
-        self.slider.setValue(0)
-
-        # Connect the slider to the playback position
-        self.slider.sliderMoved.connect(self.slider_moved)
-        self.slider.valueChanged.connect(self.update_plot_during_playback)
-        layout.addWidget(self.slider)
-
-        # Plot widget
         self.plotWidget = pg.PlotWidget()
-        layout.addWidget(self.plotWidget)
+        self.layout.addWidget(self.plotWidget)
+        self.plot_midi_data()
+
+    def initSlider(self):
+        """Initialize the slider with min/max values and its connected signals"""
+        self.slider = QSlider(Qt.Orientation.Horizontal) # The QSlider object
         
-        self.plot_midi_data() # Plot the MIDI data
+        # Set slider range + initial value
+        self.slider.setValue(0)
+        self.slider.setMinimum(0)
+        midi_length_seconds = int(self.midi_data.get_end_time())
+        self.slider.setMaximum(midi_length_seconds*10)
+
+        # Connect slider signals
+        self.slider.sliderMoved.connect(self.slider_moved)
+        self.slider.valueChanged.connect(self.slider_changed)
+        self.layout.addWidget(self.slider)
 
 
     def plot_midi_data(self):
-        """
-        Plot the MIDI data as a bar graph of pitches.
-        (Probably change to a line graph later)
-        """
-
         self.plotWidget.clear()
-        violin_df = self.instrument_dfs[40]  # Violin instrument is program 40
-
-        # Use bar graph for notes; 'stepMode=True' makes bars have equal width
+        self.violin_df = self.instrument_dfs[40]
+        self.bar_height = 1  # Fixed height for all bars
+        # Calculate the bottom position of each bar so the top aligns with the pitch
+        y_positions = self.violin_df['pitch'] - self.bar_height
         self.bars = pg.BarGraphItem(
-            x=violin_df['start'], 
-            height=violin_df['pitch'], 
-            width=violin_df['duration'], 
+            x=self.violin_df['start'], 
+            y=y_positions,  # Adjusted y position
+            height=self.bar_height,  # Fixed height
+            width=self.violin_df['duration'], 
             brush='b')
         self.plotWidget.addItem(self.bars)
 
-
     def toggle_play(self):
-        print(f'toggling play - is_playing: {self.is_playing}')
         if self.is_playing:
-            print("pausing playback")
-            pygame.mixer.music.stop()
+            pygame.mixer.music.set_pos(self.current_time)
+            pygame.mixer.music.pause()
             self.is_playing = False
             self.playButton.setText('Play')
+            self.timer.stop()
+            print('Pausing')
         else:
-            if pygame.mixer.music.get_pos() == -1 or pygame.mixer.music.get_pos() == 0:
-                print("starting playback")
-                print(f"current_time: {self.current_time}")
-                pygame.mixer.music.play()
-                print("starting playback")
-            else:
-                print("starting playback")
-                print(f"current_time: {self.current_time}")
-                pygame.mixer.music.set_pos(self.current_time)
-                pygame.mixer.music.play()
-                print("unpausing playback")
-                # pygame.mixer.music.unpause()
+            pygame.mixer.music.play()
+            pygame.mixer.music.set_pos(self.current_time)
             self.is_playing = True
-            self.playButton.setText('Pause')
             self.timer.start(100)  # Update every 100 ms
 
-
     def slider_moved(self, position):
-        self.current_time = position
-        pygame.mixer.music.set_pos(position)
-        self.plotWidget.setXRange(self.current_time-5, self.current_time+5, padding=0.1)
+        """Called when user moves the slider to [position]"""
+        self.current_slider = position
+        self.current_time = self.current_slider / 10
+        self.slider.setValue(int(self.current_slider))
+        self.update_plot()
 
+    def slider_changed(self):
+        """Called whenever slider's value changes"""
+        if self.is_playing:
+            pygame.mixer.music.set_pos(self.slider.value())
+            self.current_time = pygame.mixer.music.get_pos()
+        self.current_slider = self.current_time * 10
+        self.slider.setValue(int(self.current_slider))
+        self.update_plot()
 
-    def update_plot_during_playback(self):
-        if pygame.mixer.music.get_busy():
-            self.current_time = pygame.mixer.music.get_pos() / 1000.0  # Convert milliseconds to seconds
-            self.slider.setValue(int(self.current_time))  # Update slider position
-            self.plotWidget.setXRange(self.current_time - 5, self.current_time + 5, padding=0.1)
+    def update_slider_with_timer(self):
+        """Called at each self.timer.timeout.connect() to update the slider value"""
+        if self.is_playing:
+            self.current_slider += 1 # where 1 tick == 100 ms
+            self.current_time = self.current_slider / 10
+            self.slider.setValue(int(self.current_slider))
+            self.update_plot()
 
-    # def update_plot_during_playback(self):
-    #     if pygame.mixer.music.get_busy():
-    #         # Calculate the current playback position
-    #         self.current_time = pygame.mixer.music.get_pos() / 1000.0  # get_pos returns milliseconds
-    #         self.slider.setValue(self.df[self.df['start'] < self.current_time].index[-1])
-    #         # Update plot to reflect current playback position
-    #         self.plotWidget.setXRange(self.current_time - 5, self.current_time + 5, padding=0.1)
+    def update_plot(self):
+        """Updates the plot to reflect the current time based on the slider's position."""
+        self.plotWidget.setXRange(self.current_time - 5, self.current_time + 5, padding=0.1)
 
     def stop_midi(self):
         pygame.mixer.music.stop()
         self.timer.stop()
         self.playButton.setText('Play')
-        self.is_playing = False
+        self.is_playing
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     midi_file_path = 'mozart_vc4_mvt1.mid'  # Replace with your actual MIDI file path
     ex = MidiPlotter(midi_file_path)
     ex.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
